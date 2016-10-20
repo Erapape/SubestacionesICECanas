5b355249788be2e08e4fc8e1871771df721325a5
Erapape <erapape@gmail.com>
2016-10-19 20:05:49 -0600

    equipos

--- SubestacionesICECanas/FrmEquipos.vb
+++ SubestacionesICECanas/FrmEquipos.vb
@@ -1,3 +1,15 @@
 ﻿Public Class FrmEquipos
 
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        Me.TxtControl.Text = ""
+        Me.TxtICEControl.Text = ""
+        Me.TxtICEMarca.Text = ""
+        Me.TxtICERemota.Text = ""
+        Me.TxtLocalizacion.Text = ""
+        Me.TxtMarca.Text = ""
+        Me.TxtModelo.Text = ""
+        Me.TxtRemota.Text = ""
+        Me.TxtTipo.Text = ""
+        Me.TxtUbicacion.Text = ""
+    End Sub
 End Class
